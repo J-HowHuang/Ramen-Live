e89cb7a849fded805b8a6dc23098cc0f09142c82
J-HowHuang <howardhuang2001@gmail.com>
2021-05-16 16:09:19 +0800

    [Docker] Create docker-compose and Dockerfile for backend and DB

--- backend/main.go
+++ backend/main.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
-    "github.com/J-HowHuang/Ramen-Live/backend/pkg/websocket"
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/J-HowHuang/Ramen-Live/backend/pkg/websocket"
+	_ "github.com/joho/godotenv/autoload"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func serveWs(w http.ResponseWriter, r *http.Request) {
-    fmt.Println(r.Host)
-    ws, err := websocket.Upgrade(w, r)
-    if err != nil {
-        log.Println(err)
-    }
-    // go websocket.Writer(ws)
-    websocket.Reader(ws)
+	fmt.Println(r.Host)
+	ws, err := websocket.Upgrade(w, r)
+	if err != nil {
+		log.Println(err)
+	}
+	// go websocket.Writer(ws)
+	websocket.Reader(ws)
 }
 
 func setupRoutes() {
-    http.HandleFunc("/ws", serveWs)
+	http.HandleFunc("/ws", serveWs)
+}
+
+func connectDB() {
+	db_url := os.Getenv("DB_URL")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(db_url))
+
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	if err == nil {
+		log.Println("DB connected")
+	}
 }
 
 func main() {
-    setupRoutes()
-    http.ListenAndServe(":8088", nil)
+	setupRoutes()
+	connectDB()
+	http.ListenAndServe(":8088", nil)
+
 }
