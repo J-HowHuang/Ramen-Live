cebb7dac82394e201735ce9ff0451cf8af9d3ae6
J-HowHuang <howardhuang2001@gmail.com>
2021-05-25 22:36:12 +0800

    [Shop List] Create ShopList API

--- backend/pkg/api/getHomePage.go
+++ backend/pkg/api/getHomePage.go
@@ -2,18 +2,25 @@ package api
 
 import (
 	"log"
+
+	"github.com/J-HowHuang/Ramen-Live/backend/pkg/db"
+	"github.com/J-HowHuang/Ramen-Live/backend/pkg/loc"
 )
 
 // GetHomePage:
 
-// frontend -> {  } -> backend
+// frontend -> { userid, userlocation } -> backend
 
-// backend  -> { shopdetail([]shop) }
+// backend  -> { popularshoplist(shopid[]), nearbyshoplist[], favoriteshoplist(shopid[]) }
 
 func HandleGetHomePage(message map[string]interface{}) map[string]interface{} {
 	log.Println(message["homepageshoplist"])
-	
-	response := make(map[string]interface{})
 
-	return response
+	resp := make(map[string]interface{})
+
+	cityList := loc.GetCityList(message["userlocation"].(map[string]interface{}))
+	resp["homepageshoplist"] = loc.GetShopList(cityList)
+	resp["favoriteshoplist"] = db.GetFavoriteShopList(message["userid"].(string))
+
+	return resp
 }
